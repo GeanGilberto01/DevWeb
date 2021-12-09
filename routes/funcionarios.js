a528c787de978827d5980ab2b3e9cf7078175c8c
Gean Gilberto Coimbra <geangilertocoimbra@gmail.com>
2021-12-08 23:11:54 -0300

    CRUD

--- routes/funcionarios.js
+++ routes/funcionarios.js
@@ -14,7 +14,7 @@ connection.connect();
 /* GET funcionarios listing. */
 router.get(
     '/funcionariosGetAll',
-    (req, res, next) => {
+    (req, res) => {
         connection.query(
             'select lastname, firstname from employees limit 10',
             (error, results, fields) => {
@@ -25,5 +25,34 @@ router.get(
                 res.send(resultado);
             })
     });
+
+module.exports = router;
+
+router.get(
+    '/funcionariosGetById/:id',
+    (req, res) => {
+        var id = parseInt(req.params.id)
+        connection.query(
+            'select lastname, firstname from employees where employeeid = ?',[id],
+            (error, results) => {
+                if (error) throw error;
+                res.send(results);
+            })
+    });
+
+    router.post(
+        '/funcionariosInsert',
+        (req, res) => {
+            var firstName = req.body.FirstName
+            var lastName = req.body.LastName
+            var notes = req.body.Notes
+            connection.query(
+                'insert into employees(LastName,FirstName,Notes) values(?,?,?)',
+                [lastName,firstName,notes],
+                (error, results) => {
+                    if (error) res.send(error);
+                    else res.send(results);
+                })
+        });
 
 module.exports = router;
