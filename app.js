a851b49210ff04f11b015ce20d586489ba288bb3
Gean Gilberto Coimbra <geangilertocoimbra@gmail.com>
2021-12-08 23:45:19 -0300

    atualização rotas

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var funcionariosRouter = require('./routes/funcionarios');
+var regioesRouter = require('./routes/regioes');
+var categoriasRouter = require('./routes/categorias');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use(usersRouter);
 app.use(funcionariosRouter);
+app.use(regioesRouter);
+app.use(categoriasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
